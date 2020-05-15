0ea34d11fe27a68cc3ec8cc6903bfe25e7caf418
Philipp <pd95@users.noreply.github.com>
2020-05-15 21:25:46 +0200

    Avoid crash due to missing MOC

--- Cloud ToDo/Views/ContentView.swift
+++ Cloud ToDo/Views/ContentView.swift
@@ -133,5 +133,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, AppDelegate.shared.persistentContainer.viewContext)
     }
 }
